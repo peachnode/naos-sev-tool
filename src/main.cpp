6e431c5441d4ec1a6a74a976dd94eb4c1f4dd09d
nao <contact@peachno.de>
2023-08-02 12:11:52 +0200

    everything should be repeated 1000 times

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,9 @@
 #include <getopt.h>    // for getopt_long
 #include <stdio.h>
 #include <string>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 const char help_array[] =  "The following commands are supported:\n" \
                     " sevtool -[global opts] --[command] [command opts]\n" \
@@ -123,210 +126,224 @@ int main(int argc, char **argv)
 
     while ((c = getopt_long (argc, argv, "hio:", long_options, &option_index)) != -1)
     {
-        switch (c) {
-            case 'h':           // help
-            case 'H': {
-                printf("%s\n", help_array);
-                cmd_ret = 0;
-                break;
-            }
-            case 'i':           // sys_info
-            case 'I': {
-                Command cmd(output_folder, verbose_flag);
-                cmd_ret = cmd.sys_info();  // Display system info
-                break;
-            }
-            case 'o':           // ofolder
-            case 'O': {
-                output_folder = optarg;
-                output_folder += "/";
+        struct timespec start, end;
+        long long totalTime = 0;  // To accumulate total time
+        int repetitions = 1000;
+        for(int i = 0; i < repetitions; i++) {
+            clock_gettime(CLOCK_MONOTONIC, &start);
 
-                // Check that output folder exists, and immediately stop if not
-                std::string cmd = "if test -d " + output_folder + " ; then echo \"exist\"; else echo \"no\"; fi";
-                std::string output = "";
-                if (!sev::execute_system_command(cmd, &output)) {
-                    printf("Error. Output directory %s existance check failed.\n", output_folder.c_str());
-                    return false;
+            switch (c) {
+                case 'h':           // help
+                case 'H': {
+                    printf("%s\n", help_array);
+                    cmd_ret = 0;
+                    break;
                 }
+                case 'i':           // sys_info
+                case 'I': {
+                    Command cmd(output_folder, verbose_flag);
+                    cmd_ret = cmd.sys_info();  // Display system info
+                    break;
+                }
+                case 'o':           // ofolder
+                case 'O': {
+                    output_folder = optarg;
+                    output_folder += "/";
 
-                if (strncmp(output.c_str(), "exists", 2) != 0) {
-                    printf("Error. Output directory %s does not exist. " \
+                    // Check that output folder exists, and immediately stop if not
+                    std::string cmd = "if test -d " + output_folder + " ; then echo \"exist\"; else echo \"no\"; fi";
+                    std::string output = "";
+                    if (!sev::execute_system_command(cmd, &output)) {
+                        printf("Error. Output directory %s existance check failed.\n", output_folder.c_str());
+                        return false;
+                    }
+
+                    if (strncmp(output.c_str(), "exists", 2) != 0) {
+                        printf("Error. Output directory %s does not exist. " \
                            "Please manually create it and try again\n", output_folder.c_str());
+                        return false;
+                    }
+
+                    break;
+                }
+                case 'a': {         // PLATFORM_RESET
+                    Command cmd(output_folder, verbose_flag);
+                    cmd_ret = cmd.factory_reset();
+                    break;
+                }
+                case 'b': {         // PLATFORM_STATUS
+                    Command cmd(output_folder, verbose_flag);
+                    cmd_ret = cmd.platform_status();
+                    break;
+                }
+                case 'c': {         // PEK_GEN
+                    Command cmd(output_folder, verbose_flag);
+                    cmd_ret = cmd.pek_gen();
+                    break;
+                }
+                case 'd': {         // PEK_CSR
+                    Command cmd(output_folder, verbose_flag);
+                    cmd_ret = cmd.pek_csr();
+                    break;
+                }
+                case 'e': {         // PDH_GEN
+                    Command cmd(output_folder, verbose_flag);
+                    cmd_ret = cmd.pdh_gen();
+                    break;
+                }
+                case 'f': {         // PDH_CERT_EXPORT
+                    Command cmd(output_folder, verbose_flag);
+                    cmd_ret = cmd.pdh_cert_export();
+                    break;
+                }
+                case 'g': {         // PEK_CERT_IMPORT
+                    optind--;   // Can't use option_index because it doesn't account for '-' flags
+                    if (argc - optind != 2) {
+                        printf("Error: Expecting exactly 2 args for pek_cert_import\n");
+                        return false;
+                    }
+                    std::string signed_pek_csr_file = argv[optind++];
+                    std::string oca_cert_file = argv[optind++];
+
+                    Command cmd(output_folder, verbose_flag);
+                    cmd_ret = cmd.pek_cert_import(signed_pek_csr_file, oca_cert_file);
+                    break;
+                }
+                case 'j': {         // GET_ID
+                    Command cmd(output_folder, verbose_flag);
+                    cmd_ret = cmd.get_id();
+                    break;
+                }
+                case 'k': {         // SET_SELF_OWNED
+                    Command cmd(output_folder, verbose_flag);
+                    cmd_ret = cmd.set_self_owned();
+                    break;
+                }
+                case 'l': {         // SET_EXTERNALLY_OWNED
+                    optind--;   // Can't use option_index because it doesn't account for '-' flags
+                    if (argc - optind != 1) {
+                        printf("Error: Expecting exactly 1 arg for set_externally_owned\n");
+                        return false;
+                    }
+
+                    std::string oca_priv_key_file = argv[optind++];
+                    Command cmd(output_folder, verbose_flag);
+                    cmd_ret = cmd.set_externally_owned(oca_priv_key_file);
+                    break;
+                }
+                case 'm': {         // GENERATE_CEK_ASK
+                    Command cmd(output_folder, verbose_flag);
+                    cmd_ret = cmd.generate_cek_ask();
+                    break;
+                }
+                case 'n': {         // GET_ASK_ARK
+                    Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
+                    cmd_ret = cmd.get_ask_ark();
+                    break;
+                }
+                case 'p': {         // EXPORT_CERT_CHAIN
+                    Command cmd(output_folder, verbose_flag);
+                    cmd_ret = cmd.export_cert_chain();
+                    break;
+                }
+                case 'q': {         // EXPORT_CERT_CHAIN_VCEK
+                    Command cmd(output_folder, verbose_flag);
+                    cmd_ret = cmd.export_cert_chain_vcek();
+                    break;
+                }
+                case 's': {         // SIGN_PEK_CSR
+                    optind--;
+                    if (argc - optind != 2) {
+                        printf("Error: Expecting exactly 2 args for pek_cert_import\n");
+                        return false;
+                    }
+                    std::string pek_csr_file = argv[optind++];
+                    std::string oca_priv_key_file = argv[optind++];
+
+                    Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
+                    cmd_ret = cmd.sign_pek_csr(pek_csr_file, oca_priv_key_file);
+                    break;
+                }
+                case 't': {         // CALC_MEASUREMENT
+                    optind--;   // Can't use option_index because it doesn't account for '-' flags
+                    if (argc - optind != 8) {
+                        printf("Error: Expecting exactly 8 args for calc_measurement\n");
+                        return false;
+                    }
+
+                    measurement_t user_data;
+                    user_data.meas_ctx  = (uint8_t)strtol(argv[optind++], NULL, 16);
+                    user_data.api_major = (uint8_t)strtol(argv[optind++], NULL, 16);
+                    user_data.api_minor = (uint8_t)strtol(argv[optind++], NULL, 16);
+                    user_data.build_id  = (uint8_t)strtol(argv[optind++], NULL, 16);
+                    user_data.policy    = (uint32_t)strtol(argv[optind++], NULL, 16);
+                    sev::str_to_array(std::string(argv[optind++]), (uint8_t *)&user_data.digest, sizeof(user_data.digest));
+                    sev::str_to_array(std::string(argv[optind++]), (uint8_t *)&user_data.mnonce, sizeof(user_data.mnonce));
+                    sev::str_to_array(std::string(argv[optind++]), (uint8_t *)&user_data.tik,    sizeof(user_data.tik));
+                    Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
+                    cmd_ret = cmd.calc_measurement(&user_data);
+                    break;
+                }
+                case 'u': {         // VALIDATE_CERT_CHAIN
+                    Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
+                    cmd_ret = cmd.validate_cert_chain();
+                    break;
+                }
+                case 'v': {         // GENERATE_LAUNCH_BLOB
+                    optind--;   // Can't use option_index because it doesn't account for '-' flags
+                    if (argc - optind != 1) {
+                        printf("Error: Expecting exactly 1 arg for generate_launch_blob\n");
+                        return false;
+                    }
+
+                    uint32_t guest_policy = (uint8_t)strtol(argv[optind++], NULL, 16);
+                    Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
+                    cmd_ret = cmd.generate_launch_blob(guest_policy);
+                    break;
+                }
+                case 'w': {         // PACKAGE_SECRET
+                    Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
+                    cmd_ret = cmd.package_secret();
+                    break;
+                }
+                case 'x': {         // VALIDATE_ATTESTATION
+                    Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
+                    cmd_ret = cmd.validate_attestation();
+                    break;
+                }
+                case 'y': {         // VALIDATE_GUEST_REPORT
+                    Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
+                    cmd_ret = cmd.validate_guest_report();
+                    break;
+                }
+                case 'z': {         // VALIDATE_CERT_CHAIN_VCEK
+                    Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
+                    cmd_ret = cmd.validate_cert_chain_vcek();
+                    break;
+                }
+                case 'T': {         // Run Tests
+                    Tests test(output_folder, verbose_flag);
+                    cmd_ret = (test.test_all() == 0); // 0 = fail, 1 = pass
+                    break;
+                }
+                case 0:
+                case 1 : {
+                    // Verbose/brief
+                    break;
+                }
+                default: {
+                    fprintf(stderr, "Unrecognised option: -%c\n", optopt);
                     return false;
                 }
+            }
 
-                break;
-            }
-            case 'a': {         // PLATFORM_RESET
-                Command cmd(output_folder, verbose_flag);
-                cmd_ret = cmd.factory_reset();
-                break;
-            }
-            case 'b': {         // PLATFORM_STATUS
-                Command cmd(output_folder, verbose_flag);
-                cmd_ret = cmd.platform_status();
-                break;
-            }
-            case 'c': {         // PEK_GEN
-                Command cmd(output_folder, verbose_flag);
-                cmd_ret = cmd.pek_gen();
-                break;
-            }
-            case 'd': {         // PEK_CSR
-                Command cmd(output_folder, verbose_flag);
-                cmd_ret = cmd.pek_csr();
-                break;
-            }
-            case 'e': {         // PDH_GEN
-                Command cmd(output_folder, verbose_flag);
-                cmd_ret = cmd.pdh_gen();
-                break;
-            }
-            case 'f': {         // PDH_CERT_EXPORT
-                Command cmd(output_folder, verbose_flag);
-                cmd_ret = cmd.pdh_cert_export();
-                break;
-            }
-            case 'g': {         // PEK_CERT_IMPORT
-                optind--;   // Can't use option_index because it doesn't account for '-' flags
-                if (argc - optind != 2) {
-                    printf("Error: Expecting exactly 2 args for pek_cert_import\n");
-                    return false;
-                }
-                std::string signed_pek_csr_file = argv[optind++];
-                std::string oca_cert_file = argv[optind++];
-
-                Command cmd(output_folder, verbose_flag);
-                cmd_ret = cmd.pek_cert_import(signed_pek_csr_file, oca_cert_file);
-                break;
-            }
-            case 'j': {         // GET_ID
-                Command cmd(output_folder, verbose_flag);
-                cmd_ret = cmd.get_id();
-                break;
-            }
-            case 'k': {         // SET_SELF_OWNED
-                Command cmd(output_folder, verbose_flag);
-                cmd_ret = cmd.set_self_owned();
-                break;
-            }
-            case 'l': {         // SET_EXTERNALLY_OWNED
-                optind--;   // Can't use option_index because it doesn't account for '-' flags
-                if (argc - optind != 1) {
-                    printf("Error: Expecting exactly 1 arg for set_externally_owned\n");
-                    return false;
-                }
-
-                std::string oca_priv_key_file = argv[optind++];
-                Command cmd(output_folder, verbose_flag);
-                cmd_ret = cmd.set_externally_owned(oca_priv_key_file);
-                break;
-            }
-            case 'm': {         // GENERATE_CEK_ASK
-                Command cmd(output_folder, verbose_flag);
-                cmd_ret = cmd.generate_cek_ask();
-                break;
-            }
-            case 'n': {         // GET_ASK_ARK
-                Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
-                cmd_ret = cmd.get_ask_ark();
-                break;
-            }
-            case 'p': {         // EXPORT_CERT_CHAIN
-                Command cmd(output_folder, verbose_flag);
-                cmd_ret = cmd.export_cert_chain();
-                break;
-            }
-            case 'q': {         // EXPORT_CERT_CHAIN_VCEK
-                Command cmd(output_folder, verbose_flag);
-                cmd_ret = cmd.export_cert_chain_vcek();
-                break;
-            }
-           case 's': {         // SIGN_PEK_CSR
-                optind--;
-                if (argc - optind != 2) {
-                    printf("Error: Expecting exactly 2 args for pek_cert_import\n");
-                    return false;
-                }
-                std::string pek_csr_file = argv[optind++];
-                std::string oca_priv_key_file = argv[optind++];
-
-                Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
-                cmd_ret = cmd.sign_pek_csr(pek_csr_file, oca_priv_key_file);
-                break;
-            }
-            case 't': {         // CALC_MEASUREMENT
-                optind--;   // Can't use option_index because it doesn't account for '-' flags
-                if (argc - optind != 8) {
-                    printf("Error: Expecting exactly 8 args for calc_measurement\n");
-                    return false;
-                }
-
-                measurement_t user_data;
-                user_data.meas_ctx  = (uint8_t)strtol(argv[optind++], NULL, 16);
-                user_data.api_major = (uint8_t)strtol(argv[optind++], NULL, 16);
-                user_data.api_minor = (uint8_t)strtol(argv[optind++], NULL, 16);
-                user_data.build_id  = (uint8_t)strtol(argv[optind++], NULL, 16);
-                user_data.policy    = (uint32_t)strtol(argv[optind++], NULL, 16);
-                sev::str_to_array(std::string(argv[optind++]), (uint8_t *)&user_data.digest, sizeof(user_data.digest));
-                sev::str_to_array(std::string(argv[optind++]), (uint8_t *)&user_data.mnonce, sizeof(user_data.mnonce));
-                sev::str_to_array(std::string(argv[optind++]), (uint8_t *)&user_data.tik,    sizeof(user_data.tik));
-                Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
-                cmd_ret = cmd.calc_measurement(&user_data);
-                break;
-            }
-            case 'u': {         // VALIDATE_CERT_CHAIN
-                Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
-                cmd_ret = cmd.validate_cert_chain();
-                break;
-            }
-            case 'v': {         // GENERATE_LAUNCH_BLOB
-                optind--;   // Can't use option_index because it doesn't account for '-' flags
-                if (argc - optind != 1) {
-                    printf("Error: Expecting exactly 1 arg for generate_launch_blob\n");
-                    return false;
-                }
-
-                uint32_t guest_policy = (uint8_t)strtol(argv[optind++], NULL, 16);
-                Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
-                cmd_ret = cmd.generate_launch_blob(guest_policy);
-                break;
-            }
-            case 'w': {         // PACKAGE_SECRET
-                Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
-                cmd_ret = cmd.package_secret();
-                break;
-            }
-            case 'x': {         // VALIDATE_ATTESTATION
-                Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
-                cmd_ret = cmd.validate_attestation();
-                break;
-            }
-            case 'y': {         // VALIDATE_GUEST_REPORT
-                Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
-                cmd_ret = cmd.validate_guest_report();
-                break;
-            }
-            case 'z': {         // VALIDATE_CERT_CHAIN_VCEK
-                Command cmd(output_folder, verbose_flag, CCP_NOT_REQ);
-                cmd_ret = cmd.validate_cert_chain_vcek();
-                break;
-            }
-            case 'T': {         // Run Tests
-                Tests test(output_folder, verbose_flag);
-                cmd_ret = (test.test_all() == 0); // 0 = fail, 1 = pass
-                break;
-            }
-            case 0:
-            case 1 : {
-                // Verbose/brief
-                break;
-            }
-            default: {
-                fprintf(stderr, "Unrecognised option: -%c\n", optopt);
-                return false;
-            }
+            clock_gettime(CLOCK_MONOTONIC, &end);
+            long long timeElapsed = ((long long)end.tv_sec * 1000000000 + end.tv_nsec) - ((long long)start.tv_sec * 1000000000 + start.tv_nsec);
+            totalTime += timeElapsed;
         }
+        printf("Average time for option %c: %f ms\n", c, (totalTime / repetitions) / 1e6);
+
+
     }
 
     if (cmd_ret == 0) {
