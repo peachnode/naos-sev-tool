5e8cfbaa59a9eb9768d6c2b26ccbc190186d57dd
peachnode <contact@peachno.de>
2023-08-31 06:31:36 -0500

    mi

--- run_commands.sh
+++ run_commands.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 echo 'Executing: sevtool -r 10 --pdh_cert_export' # a lot is printed here
-sudo ./sevtool -r 10 --pdh_cert_export
+sudo ./sevtool -r 100 --pdh_cert_export
 
 echo 'Executing: sevtool -r 10 --factory_reset'
-sudo ./sevtool -r 10 --brief --factory_reset
+sudo ./sevtool -r 100 --brief --factory_reset
 
 echo 'Executing: sevtool -r 10 --platform_status'
-sudo ./sevtool -r 10 --brief --platform_status
+sudo ./sevtool -r 100 --brief --platform_status
 
 echo 'Executing: sevtool -r 10 --pdh_gen'
-sudo ./sevtool -r 10 --pdh_gen
+sudo ./sevtool -r 100 --pdh_gen
 
 echo 'Executing: sevtool -r 10 --pek_gen'
-sudo ./sevtool -r 10 --pek_gen
+sudo ./sevtool -r 100 --pek_gen
 
 echo 'Executing: sevtool -r 10 --pek_csr'
-sudo ./sevtool -r 10 --pek_csr
+sudo ./sevtool -r 100 --pek_csr
 
 echo 'Executing: sevtool -r 10 --get_id'
-sudo ./sevtool -r 10 --get_id
+sudo ./sevtool -r 100 --get_id
